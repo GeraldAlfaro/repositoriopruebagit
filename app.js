7d46b52bf5a08d2c6a75a019006f1ff5a9da6bf9
Gerald <geraldalfaro768@gmail.com>
2024-10-09 11:24:51 -0300

    contacto hecho

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none';
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        slidesPerView: 2,
+        direction: 'horizontal',
+        loop: true,
+        slidesPerGroup: 2,
+        speed: 450,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    });
+});
